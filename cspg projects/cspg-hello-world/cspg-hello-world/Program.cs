2df5efd6266c11d60e6ccff1f290fde8dafff4f0
Riddlecode <71898980+Riddlecode@users.noreply.github.com>
2021-06-11 21:43:49 -0400

    Add files via upload

---
+++ cspg projects/cspg-hello-world/cspg-hello-world/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+// Console.WriteLine("Hello World");
+//22
+//using System;
+//Console.WriteLine("Anything besides Hello World");
+//22-23
+Console.WriteLine("Hello Ritlin, I am RiddleCode");
+Console.WriteLine("I think that I can beat the Uncoded One");
+Console.WriteLine("You will need to train me though, as I seem to have lost my memory");
+Console.WriteLine("With your help, we will write the Uncoded One's destruction");
+Console.WriteLine("I think that there are an infinite number of statements I can put in a program. Will you teach me?");
